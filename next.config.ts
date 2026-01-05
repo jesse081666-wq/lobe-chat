006c7b13426cfc40c30fdf50ea6b626a7f1530d7
jesse081666-wq <jesse081666@gmail.com>
2026-01-05 23:58:50 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,12 @@ const nextConfig = defineConfig({
 
     return webpackConfig;
   },
+   typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 });
 
 export default nextConfig;
